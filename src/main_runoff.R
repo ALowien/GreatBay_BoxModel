ef0776ea119bec9a2785a8981426435f716957be
ALowien <63741311+ALowien@users.noreply.github.com>
2021-11-30 10:16:16 -0500

    coastal runoff estimation

---
+++ src/main_runoff.R
@@ -0,0 +1,54 @@
+#main_runoff.R
+
+#Author: Anna Lowien, University of New Hampshire
+#Last Updated: 11/30/2021
+
+#Purpose: Estimate load from coastal runoff using Lamprey concentrations and coastal watershed area
+
+#Load packages
+Packages <- c("readr", "dplyr", "measurements")
+
+lapply(Packages, library, character.only = TRUE)
+
+#Read in Lamprey Loads
+lmp <- read.csv("results/main_load_calc/FW_Loads/LR_Annual_Loads.csv")
+
+#Coastal Area 36km2
+GB_Coastal_Area <- 37
+
+#Load in kg/year, need to normalize it to runoff.
+#Lamprey River Watershed Area 
+
+lmp_Area <- 54897.39 #hectares
+lmp_Area_km2 <- conv_unit(lmp_Area, "hectare", "km2")
+
+#Scale lmp loads by watershed area
+
+
+lmp <- lmp %>%
+  select(-X) %>%
+  mutate(across(FW_TP:FW_TSS, ~./lmp_Area_km2))
+
+#Great Bay watershed area runoff
+
+GRB_CR <- lmp %>%
+  select(-Flow_l_year) %>%
+  mutate(across(FW_TP:FW_TSS, ~. * GB_Coastal_Area))
+
+GRB_CR <- GRB_CR %>%
+  select(-FW_TP)
+
+
+write.csv(GRB_CR, "results/main_runoff/runoff_estimate_kgyr.csv")
+
+#Repeat to get monthly coastal runoff
+lmp_monthly <- read.csv("results/Tributary_FW_Loads/Monthly_Loads/LR_MLoads_kg_month.csv")
+
+lmp_Monthly <- lmp_Monthly %>%
+  select(-X, - flow_month) %>%
+  mutate(across(FW_TP:FW_TSS, ~. /lmp_Area_km2))
+
+GRB_CR_monthly <- lmp_Monthly %>%
+  mutate(across(FW_TP:FW_TSS, ~. * GB_Coastal_Area))
+
+write.csv(GRB_CR_monthly, "results/runoff_monthly.csv")
