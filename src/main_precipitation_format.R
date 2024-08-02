57ab78ea83f91d3d12f7a3f68d1a7f637991dba1
ALowien <aelowien@gmail.com>
2024-08-01 21:33:10 -0400

    Adjustments to include through 2024

--- src/main_precipitation_format.R
+++ src/main_precipitation_format.R
@@ -11,14 +11,13 @@ Packages <- c("readxl", "dplyr", "ggplot2", "measurements", "plotly", "lubridate
 lapply(Packages, library, character.only = TRUE)
 
 #Precipitation file has been QAQC'd, correctd for MDLs, and had correct NCDC rainfall volumes matched
-precipitation <- read.csv("data/precipitation/QAQC_Precip.csv") #ALL solutes corrected for method detection limits except DON
+precipitation <- read.csv("data/precipitation/QAQC_Precip2024.csv") #ALL solutes corrected for method detection limits except DON
 
 colnames(precipitation) 
 
 precip <- precipitation %>%
-   select(UNH_ID:pH, NH4_UGL = NH4, NO3_MGL = Nitrate, PO4_UGL = PO4_Final, TDN_MGL = TDN, DOC_MGL = NPOC,
-         DON_MGL = DON, Field.Notes, Lab_Notes, TDNf:cbf) %>%
-  filter(Collection_Date < "2020-01-01")
+   select(UNH_ID:TF2_Sum_P_wUNHfilled_mm, NH4_UGL = NH4_Comb, NO3_MGL = NO3_Comb, PO4_UGL = PO4_Final, TDN_MGL = TDN, DOC_MGL = NPOC,
+         DON_MGL = DON, Field_Notes, Lab_Notes, DONf, NH4f = NH4_Combf, NO3f = NO3_Combf, TDNf, NPOCf, PO4f = PO4_Combf) 
 
 colnames(precip)
 summary(precip)
@@ -64,19 +63,19 @@ kurtosis(precip$DON_MGL_Final_MDL)
 
 #Calculate DIN
 
-precip$DIN_MGL <- precip$NO3_MGL + (precip$NH4_UGL/1000)
+precip$DIN_MGL <- ifelse(!is.na(precip$NO3_MGL) & !is.na(precip$NH4_UGL),precip$NO3_MGL + (precip$NH4_UGL/1000), NA)
 
 #Remove flagging columns now
 precip <- precip %>%
-  select(UNH_ID:Project, Rainfall_mm, pH:Lab_Notes, DON_MGL_Final_MDL, DIN_MGL, - DON_MGL)
-
+  select(UNH_ID:Project, Rainfall_mm = TF2_Sum_P_wUNHfilled_mm, NH4_UGL:DOC_MGL, DON_MGL_Final_MDL, DIN_MGL)
 summary(precip)
 
 write.csv(precip, "results/main_precipitation_format/precip_concentrations.csv")
 
 #Calculate Precipitation-Weighted Concentrations for each solute by first multiplying each measured concentration by the rainfall for that ~weekly time period
 #Create a second column for rainfall totals on days a solute was measured - accounts for weeks where there wasn't enough sample to run all of the analyses 
-precip_fw <- precip
+precip_fw <- precip %>%
+  rename(CY= Year)
 
 precip_fw$TDN_MGL_mm <- precip_fw$TDN_MGL * precip_fw$Rainfall_mm
 precip_fw$TDN_mm <- ifelse(!is.na(precip_fw$TDN_MGL), precip_fw$Rainfall_mm, NA)
@@ -103,8 +102,8 @@ precip_fw$DIN_mm <- ifelse(!is.na(precip_fw$DIN_MGL), precip_fw$Rainfall_mm, NA)
 
 #CY: Rainfall-weighted concentrations(FWC)
 precip_cy_fwc <- precip_fw %>%
-  select(-pH, - Field.Notes, -Lab_Notes) %>%
-  filter(CY > 2007 & CY < 2019) %>%
+  select(- Field_Notes, -Lab_Notes) %>%
+  filter(CY > 2007 & CY < 2024) %>%
   group_by(CY) %>%
   summarise_if(is.numeric, sum, na.rm =T) %>%
   select(CY, Rainfall_mm, TDN_MGL_mm:DIN_mm)
@@ -113,18 +112,18 @@ precip_cy_fwc <- precip_fw %>%
 #WY: Repeat the summation of columns, now grouped by water year
 
 #WY: Rainfall-weighted concentrations(FWC)
-precip_wy_fwc <- precip_fw %>%
-  select(-pH, - Field.Notes, -Lab_Notes) %>%
-  filter(WY < 2020) %>%
-  group_by(WY) %>%
-  summarise_if(is.numeric, sum, na.rm =T) %>%
-  select(WY, Rainfall_mm, TDN_MGL_mm:DIN_mm)
+#precip_wy_fwc <- precip_fw %>%
+#  select(-pH, - Field.Notes, -Lab_Notes) %>%
+#  filter(WY < 2020) %>%
+#  group_by(WY) %>%
+#  summarise_if(is.numeric, sum, na.rm =T) %>%
+ # select(WY, Rainfall_mm, TDN_MGL_mm:DIN_mm)
 
 #_________________________________________________________________________________________________________________________________________________________
 #Figure out how many samples per year
 precip_cycount <- precip_fw %>%
   select(CY, Rainfall_mm, NH4_UGL:DOC_MGL, DON_MGL_Final_MDL, DIN_MGL) %>%
-  filter(CY < 2019 & CY > 2007) %>%
+  filter(CY < 2024 & CY > 2007) %>%
   group_by(CY) %>%
   summarize(NH4_count = sum(!is.na(NH4_UGL)),
             NO3_count = sum(!is.na(NO3_MGL)),
@@ -148,21 +147,36 @@ precip_cy_finalfwc <- precip_cy_fwc %>%
 
 write.csv(precip_cy_finalfwc, "results/main_precipitation_format/precip_fwc.csv")
 
+#Table of Average Annual FWC of solutes for deposition
+precip_fw_avg_annual <- precip_cy_finalfwc %>%
+  select(FW_TDN_MGL:FW_DIN_MGL) %>%
+  mutate(FW_NH4_MGL = conv_unit(FW_NH4_UGL, "ug", "mg")) %>%
+  summarize(across(everything(), ~mean(.)))
+
+print(round(precip_fw_avg_annual[,],3))
+
+precip_fw_sd_annual <- precip_cy_finalfwc %>%
+  select(FW_TDN_MGL:FW_DIN_MGL) %>%
+  mutate(FW_NH4_MGL = conv_unit(FW_NH4_UGL, "ug", "mg")) %>%
+  summarize(across(everything(), ~sd(.)))
+
+print(round(precip_fw_sd_annual[,],2))
+
 #CY: Calculate CY average concentration and compare to precip-weighted concentration 
 precip_cy_avg_conc <- precip_fw %>%
   group_by(CY) %>%
-  summarize(across(.cols=everything(), mean, na.rm=T))
+  summarize(across(NH4_UGL:DIN_MGL, \(x) mean(x, na.rm=TRUE)))
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #WY: Divide the summation of C*Rainfall by the summation of rainfall for each solute
-precip_wy_finalfwc <- precip_wy_fwc %>%
-  mutate(FW_TDN_MGL = TDN_MGL_mm/TDN_mm,
-         FW_DOC_MGL = DOC_MGL_mm/DOC_mm,
-         FW_NO3_MGL = NO3_MGL_mm/NO3_mm,
-         FW_NH4_UGL = NH4_UGL_mm/NH4_mm,
-         FW_PO4_UGL = PO4_UGL_mm/PO4_mm,
-         FW_DON_MGL = DON_MGL_mm/DON_mm,
-         FW_DIN_MGL = DIN_MGL_mm/DIN_mm)
+#precip_wy_finalfwc <- precip_wy_fwc %>%
+ # mutate(FW_TDN_MGL = TDN_MGL_mm/TDN_mm,
+  #       FW_DOC_MGL = DOC_MGL_mm/DOC_mm,
+   #      FW_NO3_MGL = NO3_MGL_mm/NO3_mm,
+    #     FW_NH4_UGL = NH4_UGL_mm/NH4_mm,
+     #    FW_PO4_UGL = PO4_UGL_mm/PO4_mm,
+      #   FW_DON_MGL = DON_MGL_mm/DON_mm,
+       #  FW_DIN_MGL = DIN_MGL_mm/DIN_mm)
 
 #WY: Calculate WY average concentration and compare to precip-weighted concentration
 #precip_wy_Avg_Conc <- precip_fw %>%
@@ -175,13 +189,15 @@ precip_wy_finalfwc <- precip_wy_fwc %>%
 
 #CY: Product of Precip-Weighted Annual Concentration (mg/L) and Annual rainfall (mm)
 #Calendar Year TF2 Rainfall Totals (mm rainfall)
-cy_rainfall <- read_excel("data/precipitation/TF2_CalYr_WaterYr_precipsum.xlsx", sheet =3) %>%
-  filter(CY >2007 & CY < 2019)
+cy_rainfall <- read.csv("./data/precipitation/CY_Rainfalll_24update.csv") %>%
+  select(CY= year, Rainfall_mm = total_rainfall_mm) %>%
+  filter(CY >2007 & CY < 2024)
 
 #Plot Rainfall
 p_rainfall_cy <- ggplot(cy_rainfall, aes(CY, Rainfall_mm)) + geom_point() + 
-  scale_x_continuous(breaks=seq(from=2008,to=2018,by=1)) + 
-  scale_y_continuous(limits=c(0,1800), breaks=seq(from=0,to=1800, by=200))+ theme_cowplot()
+  scale_x_continuous(breaks=seq(from=2008,to=2023,by=1)) + 
+  scale_y_continuous(limits=c(0,1800), breaks=seq(from=0,to=1800, by=200))+ 
+  theme_cowplot()
 p_rainfall_cy 
 
 #CY: Convert concentrations from mg/L or ug/L to kg/L
@@ -223,44 +239,47 @@ conv_unit(1, "mm2", "hectare")
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #Water Year TF2 Rainfall Totals (mm rainfall)
-wy_rainfall <- read_excel("data/precipitation/TF2_CalYr_WaterYr_precipsum.xlsx", sheet =4) %>%
-  filter(WY > 2007 & WY < 2019)
+#wy_rainfall <- read_excel("data/precipitation/TF2_CalYr_WaterYr_precipsum.xlsx", sheet =4) %>%
+ # filter(WY > 2007 & WY < 2019)
 
 
 #WY: Convert concentrations from mg/L or ug/L to kg/L
-precip_wy_loads <- precip_wy_finalfwc %>%
-  select(WY, FW_TDN_MGL:FW_DIN_MGL) %>%
-  mutate(FW_TDN_KGL = conv_unit(FW_TDN_MGL, "mg", "kg"),
-         FW_DOC_KGL = conv_unit(FW_DOC_MGL, "mg", "kg"),
-         FW_NO3_KGL = conv_unit(FW_NO3_MGL, "mg", "kg"),
-         FW_NH4_KGL = conv_unit(FW_NH4_UGL, "ug", "kg"),
-         FW_PO4_KGL = conv_unit(FW_PO4_UGL, "ug", "kg"),
-         FW_DON_KGL = conv_unit(FW_DON_MGL, "mg", "kg"),
-         FW_DIN_KGL = conv_unit(FW_DIN_MGL, "mg", "kg"))
+#precip_wy_loads <- precip_wy_finalfwc %>%
+ # select(WY, FW_TDN_MGL:FW_DIN_MGL) %>%
+#  mutate(FW_TDN_KGL = conv_unit(FW_TDN_MGL, "mg", "kg"),
+ #        FW_DOC_KGL = conv_unit(FW_DOC_MGL, "mg", "kg"),
+  #       FW_NO3_KGL = conv_unit(FW_NO3_MGL, "mg", "kg"),
+  #      FW_NH4_KGL = conv_unit(FW_NH4_UGL, "ug", "kg"),
+  #       FW_PO4_KGL = conv_unit(FW_PO4_UGL, "ug", "kg"),
+  #       FW_DON_KGL = conv_unit(FW_DON_MGL, "mg", "kg"),
+  #       FW_DIN_KGL = conv_unit(FW_DIN_MGL, "mg", "kg"))
 
 #WY: Join the precip-weighted concentrations dataframe with the annual rainfall dataframe by water year
-precip_wy_loads <- full_join(precip_wy_loads, wy_rainfall, by = "WY")
+
+#precip_wy_loads <- full_join(precip_wy_loads, wy_rainfall, by = "WY")
 
 #1L H20 = 1,000,000 mm^3 H20
 #WY:Calculate Loads in kg/mm2/yr (divide by 1000000 mm3 to get kg/mm3 * mm/year = kg/mm2/year)
-precip_wy_loads <- precip_wy_loads %>%
-  mutate(TDN_kg_mm2_yr = (FW_TDN_KGL/1000000) * Rainfall_mm,
-         DOC_kg_mm2_yr = (FW_DOC_KGL/1000000) * Rainfall_mm,
-         NO3_kg_mm2_yr = (FW_NO3_KGL/1000000) * Rainfall_mm,
-         NH4_kg_mm2_yr = (FW_NH4_KGL/1000000) * Rainfall_mm,
-         PO4_kg_mm2_yr = (FW_PO4_KGL/1000000) * Rainfall_mm,
-         DIN_kg_mm2_yr = (FW_DIN_KGL/1000000) * Rainfall_mm,
-         DON_kg_mm2_yr = (FW_DON_KGL/1000000) * Rainfall_mm) # divide by 1000000 mm3 to get kg/mm3 * mm/year = kg/mm2/year
+
+#precip_wy_loads <- precip_wy_loads %>%
+ # mutate(TDN_kg_mm2_yr = (FW_TDN_KGL/1000000) * Rainfall_mm,
+  #       DOC_kg_mm2_yr = (FW_DOC_KGL/1000000) * Rainfall_mm,
+  #      NO3_kg_mm2_yr = (FW_NO3_KGL/1000000) * Rainfall_mm,
+  #       NH4_kg_mm2_yr = (FW_NH4_KGL/1000000) * Rainfall_mm,
+  #       PO4_kg_mm2_yr = (FW_PO4_KGL/1000000) * Rainfall_mm,
+  #       DIN_kg_mm2_yr = (FW_DIN_KGL/1000000) * Rainfall_mm,
+  #       DON_kg_mm2_yr = (FW_DON_KGL/1000000) * Rainfall_mm) # divide by 1000000 mm3 to get kg/mm3 * mm/year = kg/mm2/year
 
 #WY: Convert Loads from kg/mm^2/yr to kg/ha/yr by multiplying by 1e^10 mm2 in 1 ha
-precip_wy_loads <- precip_wy_loads %>%
-  mutate(TDN_kg_ha_yr = TDN_kg_mm2_yr * (1*10^10),
-         DOC_kg_ha_yr = DOC_kg_mm2_yr * (1*10^10),
-         NO3_kg_ha_yr = NO3_kg_mm2_yr * (1*10^10),
-         NH4_kg_ha_yr = NH4_kg_mm2_yr * (1*10^10),
-         PO4_kg_ha_yr = PO4_kg_mm2_yr * (1*10^10),
-         DIN_kg_ha_yr = DIN_kg_mm2_yr * (1*10^10),
-         DON_kg_ha_yr = DON_kg_mm2_yr * (1*10^10))
+
+#precip_wy_loads <- precip_wy_loads %>%
+ # mutate(TDN_kg_ha_yr = TDN_kg_mm2_yr * (1*10^10),
+  #       DOC_kg_ha_yr = DOC_kg_mm2_yr * (1*10^10),
+  #       NO3_kg_ha_yr = NO3_kg_mm2_yr * (1*10^10),
+  #      NH4_kg_ha_yr = NH4_kg_mm2_yr * (1*10^10),
+  #       PO4_kg_ha_yr = PO4_kg_mm2_yr * (1*10^10),
+  #       DIN_kg_ha_yr = DIN_kg_mm2_yr * (1*10^10),
+  #      DON_kg_ha_yr = DON_kg_mm2_yr * (1*10^10))
 
 #___________________________________________________________________________________________________________________________________________________________________________
 #Create final tables of CY and WY loads
@@ -286,20 +305,19 @@ precip_cy_loads_final <- precip_cy_loads_final %>%
          DON_kg_yr = DON_kg_ha_yr * GB_Area_ha)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #WY: Loads table
-precip_wy_loads_final <- precip_wy_loads %>%
-  select(WY, Rainfall_mm, TDN_kg_ha_yr:DON_kg_ha_yr)
+#precip_wy_loads_final <- precip_wy_loads %>%  select(WY, Rainfall_mm, TDN_kg_ha_yr:DON_kg_ha_yr)
 
 #Multiply normalized area by Great Bay surface area
 
-precip_wy_loads_final <- precip_wy_loads_final %>%
-  mutate(TDN_kg_yr = TDN_kg_ha_yr * GB_Area_ha,
-         DOC_kg_yr = DOC_kg_ha_yr * GB_Area_ha,
-         NO3_kg_yr = NO3_kg_ha_yr * GB_Area_ha,
-         NH4_kg_yr = NH4_kg_ha_yr * GB_Area_ha,
-         PO4_kg_yr = PO4_kg_ha_yr * GB_Area_ha,
-         DIN_kg_yr = DIN_kg_ha_yr * GB_Area_ha,
-         DON_kg_yr = DON_kg_ha_yr * GB_Area_ha) %>%
-  filter(WY < 2019)
+#precip_wy_loads_final <- precip_wy_loads_final %>%
+ # mutate(TDN_kg_yr = TDN_kg_ha_yr * GB_Area_ha,
+ #        DOC_kg_yr = DOC_kg_ha_yr * GB_Area_ha,
+#         NO3_kg_yr = NO3_kg_ha_yr * GB_Area_ha,
+#         NH4_kg_yr = NH4_kg_ha_yr * GB_Area_ha,
+#         PO4_kg_yr = PO4_kg_ha_yr * GB_Area_ha,
+#         DIN_kg_yr = DIN_kg_ha_yr * GB_Area_ha,
+#         DON_kg_yr = DON_kg_ha_yr * GB_Area_ha) %>%
+#  filter(WY < 2019)
 
 #___________________________________________________________________________________________________________________________________________
 #Plot loads over time
@@ -313,18 +331,18 @@ cy_loads_final_long <- precip_cy_loads_final %>%
                values_to = "Load_kg_yr")
 
 plot_cy_loads <- ggplot(cy_loads_final_long, aes(CY, Load_kg_yr)) + geom_point(aes(color=Solute), size = 3) +
-  scale_x_continuous(limits=c(2008,2018), breaks=seq(from=2008, to=2018, by= 2)) + 
+  scale_x_continuous(limits=c(2008,2023), breaks=seq(from=2008, to=2024, by= 1)) + 
   scale_color_viridis_d() +
   facet_wrap(~Solute, scales="free") + theme_cowplot()
 plot_cy_loads
 
 #Pivot the WY Loads table to be long
 
-wy_loads_final_long <- precip_wy_loads_final %>%
-  select(WY, TDN_kg_yr:DON_kg_yr) %>%
-  pivot_longer(cols =  TDN_kg_yr:DON_kg_yr,
-               names_to = "Solute",
-               values_to = "Load_kg_yr")
+#wy_loads_final_long <- precip_wy_loads_final %>%
+ # select(WY, TDN_kg_yr:DON_kg_yr) %>%
+  #pivot_longer(cols =  TDN_kg_yr:DON_kg_yr,
+   #            names_to = "Solute",
+    #           values_to = "Load_kg_yr")
 
 #_______________________________________________________________________________________________________________________________________________
 #Average of each concentration over the decade long period of record
@@ -400,24 +418,28 @@ precip_month_loads <- precip_month_fwc_final %>%
          FW_DIN_KGL = conv_unit(FW_DIN_MGL, "mg", "kg"))
 
 
-#Add in Rainfall
-monthly_precip_mm <- read_excel("data/precipitation/Monthly_Precipitation.xlsx")
+#Add in Monthly Rainfall
+monthly_precip_mm <- read.csv("data/precipitation/Monthly_Rainfall_24update.csv") %>%
+  rename(Rainfall_mm = total_rainfall_mm, 
+         Month = month,
+         Year = year) %>% 
+  select(-X)
 
 monthly_precip_mm<- monthly_precip_mm %>%
-  select(Month_Year, CY=Year, Month, Rainfall_mm)
+  select(CY=Year, Month, Rainfall_mm)
 
-monthly_precip_mm$Month <- ifelse(monthly_precip_mm$Month == "JAN", 1, 
-                                  ifelse(monthly_precip_mm$Month == "FEB", 2,
-                                         ifelse(monthly_precip_mm$Month == "MAR", 3,
-                                                ifelse(monthly_precip_mm$Month == "APR", 4,
-                                                       ifelse(monthly_precip_mm$Month == "MAY", 5,
-                                                              ifelse(monthly_precip_mm$Month == "JUN", 6,
-                                                                     ifelse(monthly_precip_mm$Month == "JUL", 7,
-                                                                            ifelse(monthly_precip_mm$Month == "AUG", 8,
-                                                                                   ifelse(monthly_precip_mm$Month == "SEP", 9,
-                                                                                          ifelse(monthly_precip_mm$Month == "OCT", 10,
-                                                                                                 ifelse(monthly_precip_mm$Month == "NOV", 11,
-                                                                                                        ifelse(monthly_precip_mm$Month == "DEC", 12, NA))))))))))))
+#monthly_precip_mm$Month <- ifelse(monthly_precip_mm$Month == "JAN", 1, 
+ #                                 ifelse(monthly_precip_mm$Month == "FEB", 2,
+  #                                       ifelse(monthly_precip_mm$Month == "MAR", 3,
+   #                                             ifelse(monthly_precip_mm$Month == "APR", 4,
+    #                                                   ifelse(monthly_precip_mm$Month == "MAY", 5,
+     #                                                         ifelse(monthly_precip_mm$Month == "JUN", 6,
+      #                                                               ifelse(monthly_precip_mm$Month == "JUL", 7,
+       #                                                                     ifelse(monthly_precip_mm$Month == "AUG", 8,
+        #                                                                           ifelse(monthly_precip_mm$Month == "SEP", 9,
+                                                                          #               ifelse(monthly_precip_mm$Month == "OCT", 10,
+                                                                                          #       ifelse(monthly_precip_mm$Month == "NOV", 11,
+                                                                                            #            ifelse(monthly_precip_mm$Month == "DEC", 12, NA))))))))))))
 
 
 #Merge Monthly Rainfall with Monthly flow-weighted concentrations
@@ -426,7 +448,7 @@ colnames(precip_month_loads)
 
 precip_month_loads <- full_join(precip_month_loads, monthly_precip_mm, by=c("CY", "Month")) %>%
   select(CY, Month, FW_TDN_KGL:FW_DIN_KGL, Rainfall_mm) %>%
-  filter(CY < 2019 & CY > 2007)
+  filter(CY < 2024 & CY > 2007)
 
 #1L H20 = 1,000,000 mm^3 H20
 #Month:Calculate Loads in kg/mm2/month (divide by 1000000 mm3 to get kg/mm3 * mm/month = kg/mm2/month)
@@ -467,17 +489,17 @@ precip_month_loads_final <- precip_month_loads_final %>%
 precip_cy_loads_final$TN_kg_yr <- precip_cy_loads_final$TDN_kg_yr
 precip_cy_loads_final$TN_kg_ha_yr <- precip_cy_loads_final$TDN_kg_ha_yr
 
-precip_wy_loads_final$TN_kg_yr <- precip_wy_loads_final$TDN_kg_yr
-precip_wy_loads_final$TN_kg_ha_yr <- precip_wy_loads_final$TDN_kg_ha_yr
+#precip_wy_loads_final$TN_kg_yr <- precip_wy_loads_final$TDN_kg_yr
+#precip_wy_loads_final$TN_kg_ha_yr <- precip_wy_loads_final$TDN_kg_ha_yr
 
 precip_month_loads_final$TN_kg_month <- precip_month_loads_final$TDN_kg_month
 precip_month_loads_final$TN_kg_ha_month <- precip_month_loads_final$TDN_kg_ha_month
 
 #Write 3 csv files: WY Loads, CY Loads, and Monthly Loads
 
-write.csv(precip_cy_loads_final, "results/main_precipitation_format/cy_precip_loads.csv")
-write.csv(precip_wy_loads_final, "results/main_precipitation_format/wy_precip_loads.csv")
-write.csv(precip_month_loads_final, "results/main_precipitation_format/month_precip_loads.csv")
+write.csv(precip_cy_loads_final, "results/main_precipitation_format/cy_precip_loads24.csv")
+#write.csv(precip_wy_loads_final, "results/main_precipitation_format/wy_precip_loads.csv")
+write.csv(precip_month_loads_final, "results/main_precipitation_format/month_precip_loads24.csv")
 
 
 write.csv(cy_rainfall, "results/main_precipitation_format/CY_Rainfall.csv")
