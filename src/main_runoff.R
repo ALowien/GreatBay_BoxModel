80e0e6eb9e9e52d07f22d0e22db810209938a876
ALowien <aelowien@gmail.com>
2025-02-21 10:48:35 -0500

    adjusted coastal drainage area based on maps measurements

--- src/main_runoff.R
+++ src/main_runoff.R
@@ -13,29 +13,21 @@ lapply(Packages, library, character.only = TRUE)
 #Read in Lamprey Riverine Loads
 lmp <- read.csv("results/main_load_calc/FW_Loads/LR_Annual_Loads.csv")
 
-#Coastal Area 36km2
-GB_Coastal_Area <- 37 #estimated using Google Earth mapping tools
-
+#Coastal Area 42km2 (based on map calculations) - does not include water surface or any of the tidal tributary watershed area
+GB_Coastal_Area <- 42 #km2
 #Load in kg/year, need to normalize it to runoff.
 #Lamprey River Watershed Area 
-
-lmp_Area <- 54897.39 #hectares
-lmp_Area_km2 <- conv_unit(lmp_Area, "hectare", "km2")
+lmp_Area_km2 <- 554
 
 #Scale lmp loads by watershed area
 lmp <- lmp %>%
   select(-X) %>%
-  mutate(across(FW_TP:FW_TSS, ~./lmp_Area_km2))
+  mutate(across(FW_PO4:FW_TSS, ~./lmp_Area_km2))
 
 #Great Bay watershed area runoff
-
 GRB_CR <- lmp %>%
   select(-Flow_l_year) %>%
-  mutate(across(FW_TP:FW_TSS, ~. * GB_Coastal_Area))
-
-GRB_CR <- GRB_CR %>%
-  select(-FW_TP)
-
+  mutate(across(FW_PO4:FW_TSS, ~. * GB_Coastal_Area))
 
 write.csv(GRB_CR, "results/main_runoff/runoff_estimate_kgyr.csv")
 
@@ -44,9 +36,10 @@ lmp_monthly <- read.csv("results/main_load_calc/FW_Loads/LR_MLoads_kg_month.csv"
 
 lmp_monthly <- lmp_monthly %>%
   select(-X, - flow_month) %>%
-  mutate(across(FW_TP:FW_TSS, ~. /lmp_Area_km2))
+  mutate(across(FW_PO4:FW_TSS, ~. /lmp_Area_km2))
 
 GRB_CR_monthly <- lmp_monthly %>%
-  mutate(across(FW_TP:FW_TSS, ~. * GB_Coastal_Area))
+  mutate(across(FW_PO4:FW_TSS, ~. * GB_Coastal_Area))
 
-write.csv(GRB_CR_monthly, "results/main_runoff/runoff_monthly.csv")
+write.csv(GRB_CR_monthly, "results/main_runoff/runoff_estimate_kgmonth.csv")
+ 
